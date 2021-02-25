34987c5b68c58194c5c42c7356c9ea858cc2fcae
aschi2403 <jakob@familie-aschauer.eu>
2021-02-25 16:28:04 +0100

    implement navigation from Settings to legal_notice

--- Android/app/src/main/java/aschi2403/tsiy/screens/SettingsFragment.kt
+++ Android/app/src/main/java/aschi2403/tsiy/screens/SettingsFragment.kt
@@ -11,6 +11,7 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import androidx.core.widget.doOnTextChanged
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import aschi2403.tsiy.Legal_NoticeFragment
 import aschi2403.tsiy.R
 import aschi2403.tsiy.databinding.FragmentSettingsBinding
@@ -67,7 +68,7 @@ class SettingsFragment : Fragment() {
 
 
     private fun legal_notice() {
-
+        findNavController().navigate(R.id.action_settingsFragment_to_legal_NoticeFragment)
     }
 
     override fun onStop() {
