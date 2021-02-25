69f967cd8f7611f2ce699e6065e68d15b8279983
aschi2403 <jakob@familie-aschauer.eu>
2021-02-25 17:26:26 +0100

    add navigation from WeightFragment to AddWeightFragment

--- Android/app/src/main/java/aschi2403/tsiy/AddWeightFragment.kt
+++ Android/app/src/main/java/aschi2403/tsiy/AddWeightFragment.kt
@@ -1,35 +1,29 @@
 package aschi2403.tsiy
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import aschi2403.tsiy.databinding.FragmentAddWeightBinding
 import aschi2403.tsiy.databinding.FragmentWeightBinding
 
-
-class WeightFragment : Fragment() {
-    private lateinit var binding: FragmentWeightBinding
+/**
+ * A simple [Fragment] subclass.
+ * Use the [AddWeightFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AddWeightFragment : Fragment() {
+    private lateinit var binding: FragmentAddWeightBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_weight, container, false
+            inflater, R.layout.fragment_add_weight, container, false
         )
-
-        binding.addWeight.setOnClickListener {
-            addWeight()
-        }
-
         return binding.root
     }
-
-    private fun addWeight() {
-        findNavController().navigate(R.id.action_weightFragment_to_addWeightFragment)
-    }
-
 }
