947ab6c4032fd9cd07b7e1838e54d02503029196
aschi2403 <jakob@familie-aschauer.eu>
2021-02-26 15:52:18 +0100

    added UI to add Weight entry, implement functionality to save new weight entry to database

--- Android/app/src/main/java/aschi2403/tsiy/screens/fragments/AddWeightFragment.kt
+++ Android/app/src/main/java/aschi2403/tsiy/screens/fragments/AddWeightFragment.kt
@@ -6,8 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import aschi2403.tsiy.R
 import aschi2403.tsiy.databinding.FragmentAddWeightBinding
+import aschi2403.tsiy.model.WeightEntry
+import aschi2403.tsiy.repository.WorkoutRepo
+import kotlinx.android.synthetic.main.fragment_add_weight.*
 
 /**
  * A simple [Fragment] subclass.
@@ -16,6 +20,7 @@ import aschi2403.tsiy.databinding.FragmentAddWeightBinding
  */
 class AddWeightFragment : Fragment() {
     private lateinit var binding: FragmentAddWeightBinding
+    private var repo: WorkoutRepo? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +30,30 @@ class AddWeightFragment : Fragment() {
             inflater,
             R.layout.fragment_add_weight, container, false
         )
+
+        binding.confirmButton.setOnClickListener { confirm_button() }
+
+        repo = activity?.let { WorkoutRepo(it) }
+
         return binding.root
+    }
+
+    //handler for onClick of confirm button
+    fun confirm_button() {
+        // get text if input field
+        val text: String = binding.weightValue.text.toString()
+
+        // parse value
+        val value: Double? = text.toDoubleOrNull()
+
+        // if value is valid add new database entry
+        if (value != null) {
+            val weightEntry = WeightEntry(date = System.currentTimeMillis(), weight = value)
+
+            repo?.addWeightEntry(weightEntry)
+        }
+
+        // navigate back to weight-fragment
+        findNavController().navigate(R.id.action_addWeightFragment_to_weightFragment)
     }
 }
