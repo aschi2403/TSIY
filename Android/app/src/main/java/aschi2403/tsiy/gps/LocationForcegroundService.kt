01afed908438642c99f3519fdc1740190d4c7d95
Alexander Schoenberger <alexander@schoenberger.cc>
2021-09-14 19:40:52 +0200

    added changes for pull request

--- Android/app/src/main/java/aschi2403/tsiy/gps/LocationForcegroundService.kt
+++ Android/app/src/main/java/aschi2403/tsiy/gps/LocationForcegroundService.kt
@@ -5,16 +5,19 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.Build
 import android.os.IBinder
 import androidx.annotation.RequiresApi
 
 
-class LocationForceGroundService: Service() {
+class LocationForceGroundService : Service() {
     private lateinit var locationProvider: LocationProvider
 
+    inner class LocationForceGroundServiceBinder : Binder() {
+        fun getService() = this@LocationForceGroundService
+    }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate() {
         super.onCreate()
 
@@ -23,17 +26,15 @@ class LocationForceGroundService: Service() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val serviceChannel =
                 NotificationChannel("TSIYLocationChannel", "TSIY Location Provider", NotificationManager.IMPORTANCE_LOW)
-                getSystemService(NotificationManager::class.java).createNotificationChannel(serviceChannel)
+            getSystemService(NotificationManager::class.java).createNotificationChannel(serviceChannel)
 
             startForeground(134, Notification.Builder(baseContext, "TSIYLocationChannel").build())
         }
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return START_NOT_STICKY
-    }
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int = START_NOT_STICKY
 
-    override fun onBind(intent: Intent?): IBinder? {
-        return null
+    override fun onBind(intent: Intent?): IBinder {
+        return LocationForceGroundServiceBinder()
     }
 }
